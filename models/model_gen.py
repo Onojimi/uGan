df8526a00745d00cd454263d9536fc55df41fe93
Onojimi <18811505578@163.com>
2019-08-20 09:41:26 -0400

    Update model_gen.py

--- models/model_gen.py
+++ models/model_gen.py
@@ -71,7 +71,7 @@ def init_weights(net, init_type = 'normal', gain = 0.002):
 def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
     if len(gpu_ids) > 0:
         assert(torch.cuda.is_available())
-        net.to(gpu_ids[0])
+        net.to('cuda:'+gpu_ids[0])
         net = nn.DataParallel(net, gpu_ids)
     init_weights(net, init_type, gain=init_gain)
     return net  

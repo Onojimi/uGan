6a30d2a3c0d6c2de450dd807243d7fc0b8c499fa
Onojimi <18811505578@163.com>
2019-08-21 16:00:36 -0400

    Update data.py

--- data.py
+++ data.py
@@ -3,13 +3,13 @@ from os.path import join
 from dataset import DatasetFromFolder
 
 
-def get_training_set(root_dir, direction):
+def get_training_set(root_dir):
     train_dir = join(root_dir, "train")
 
-    return DatasetFromFolder(train_dir, direction)
+    return DatasetFromFolder(train_dir)
 
 
-def get_val_set(root_dir, direction):
+def get_val_set(root_dir):
     val_dir = join(root_dir, "val")
 
-    return DatasetFromFolder(val_dir, direction)
+    return DatasetFromFolder(val_dir)

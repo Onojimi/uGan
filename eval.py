e362d4b836c237ee9e8b0d17d088bfd02c0cfa40
Onojimi <18811505578@163.com>
2019-08-21 11:21:43 -0400

    Update eval.py

--- eval.py
+++ eval.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def compute_iou(true, pred):
     true_mask = np.asanyarray(true.cpu(), dtype = np.bool)
-    pred_mask = np.asanyarray(pred, dtype = np.bool)
+    pred_mask = np.asanyarray(pred.cpu(), dtype = np.bool)
     union = np.sum(np.logical_or(true_mask, pred_mask))
     intersection = np.sum(np.logical_and(true_mask, pred_mask))
 #    print(union, intersection)
